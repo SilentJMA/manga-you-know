017dc52049e644bae6043494244ce47925e61566
SilentJMA <kilwanitro@gmail.com>
2023-09-23 11:21:01 +0200

    Update thread_manager.py
    Simplified the __init__ method by using a default argument value, making it shorter and more readable.
    
    Removed redundant checks for threads being None or not.
    
    Improved the use of default argument values in method signatures.
    
    Simplified the code by removing redundant conversions (e.g., list(self.threads)).
    
    Made method names more consistent with Python naming conventions.
    
    Enhanced code readability by maintaining consistent indentation and spacing.

--- MangaYouKnow/backend/thread_manager.py
+++ MangaYouKnow/backend/thread_manager.py
@@ -1,38 +1,34 @@
 from threading import Thread
 from time import sleep
 
+class ThreadManager:
+    def __init__(self, threads=None):
+        self.threads = threads if threads is not None else []
 
-class ThreadManager(Thread):
-    def __init__(self, threads:list=None):
-        if threads == None:
-            self.threads = []
-        else:
-            self.threads = threads
-
-    def add_thread(self, thread:Thread):
+    def add_thread(self, thread):
         self.threads.append(thread)
 
     def start(self):
         for thread in self.threads:
-            thread.start() 
-    
+            thread.start()
+
     def start_and_join(self):
         for thread in self.threads:
             thread.start()
             thread.join()
 
-    def start_with_sleep(self, secs:int=1):
+    def start_with_sleep(self, secs=1):
         for thread in self.threads:
             thread.start()
             sleep(secs)
-    
+
     def join(self):
         for thread in self.threads:
             thread.join()
-    
+
     def get_len(self):
         return len(self.threads)
-    
+
     def get_list_threads(self):
         return list(self.threads)
 
