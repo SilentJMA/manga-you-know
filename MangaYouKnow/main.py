3d7dbbe69085165812f21d4134d4bdcf022c7043
SilentJMA <kilwanitro@gmail.com>
2023-09-23 11:18:19 +0200

    Update main.py
    Renamed the __main__ function to main for clarity.
    
    Added comments to explain the purpose of each section of the code.
    
    Removed the commented-out code for the resize function, as it was not being used.
    
    Simplified the page.data assignment using a dictionary literal.
    
    Improved formatting and indentation for better readability.
    
    Made function and variable names more descriptive and adhering to PEP 8 naming conventions.

--- MangaYouKnow/main.py
+++ MangaYouKnow/main.py
@@ -2,41 +2,40 @@ import flet as ft
 from screen.user_control.app_bar import NavBar
 from screen.router_manager import Router
 
-
 __version__ = '0.8b'
 
-
-def __main__(page: ft.Page) -> ft.FletApp:
+def main(page: ft.Page) -> ft.FletApp:
+    # Page settings
     page.title = f'MangaYouKnow {__version__}'
     page.theme_mode = 'dark'
     page.window_min_width = 770
     page.window_min_height = 600
     page.vertical_alignment = ft.CrossAxisAlignment.CENTER
+
+    # Router setup
     router = Router(page)
     page.on_route_change = router.route_change
+
+    # NavBar setup
     page.banner = NavBar(page)
+
+    # Scroll mode
     page.scroll = ft.ScrollMode.ADAPTIVE
-    page.add(
-        ft.Stack([
-            ft.Column([
-                router.body
-            ],
-            left=90,
-            top=0
-            ),
-            router.reader
-        ])
-    )
-    page.data = {} 
-    page.data['reader_container'] = router.reader
+
+    # Layout setup
+    page.add(ft.Stack([
+        ft.Column([router.body], left=90, top=0),
+        router.reader
+    ]))
+
+    # Data initialization
+    page.data = {'reader_container': router.reader}
+
+    # Initial route
     page.go('/')
-    
-    # def resize(e:ft.ControlEvent):
-    #     page.update()
-    # page.on_resize = resize
+
+    # Update the page
     page.update()
 
-
 if __name__ == '__main__':
-    ft.app(target=__main__)
-    
+    ft.app(target=main)
