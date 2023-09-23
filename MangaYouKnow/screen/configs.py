b995be69f03904cee2535f52c14621785bac952e
SilentJMA <kilwanitro@gmail.com>
2023-09-23 11:17:46 +0200

    Update configs.py
    Created an instance variable self.page to store the page argument for easier access within the class methods.
    
    Moved the initialization of keybinds to the class constructor to avoid repeated database queries.
    
    Created a separate method create_keybind_buttons to generate keybind buttons, making the code more modular and readable.
    
    Simplified the creation of keybind buttons using a loop and added lambda functions to handle button clicks.
    
    Enhanced the change_keys_content creation by using a list comprehension for better readability.
    
    Moved the creation and opening of the change_keys_dialog outside of the change_keybind function, making it more organized.
    
    Added comments for better code documentation.

--- MangaYouKnow/screen/configs.py
+++ MangaYouKnow/screen/configs.py
@@ -3,44 +3,43 @@ from backend.database import DataBase
 
 class Configs:
     def __init__(self, page: ft.Page):
-        database = DataBase()
-        def change_keybind(_=None):
-            keybinds = database.get_config()['keybinds']
-            def listen_key(btn:ft.TextButton):
-                btn.text = '...'
-                page.update()
-            full_screen = ft.TextButton(keybinds['full-screen'], disabled=True)
-            full_screen.on_click = lambda e, btn=full_screen: listen_key(btn)
-            return_home = ft.TextButton(keybinds['return-home'], disabled=True)
-            return_home.on_click = lambda e, btn=return_home: listen_key(btn)
-            next_page = ft.TextButton(keybinds['next-page'], disabled=True)
-            next_page.on_click = lambda e, btn=next_page: listen_key(btn)
-            previous_page = ft.TextButton(keybinds['previous-page'], disabled=True)
-            previous_page.on_click = lambda e, btn=previous_page: listen_key(btn)
-            
-            change_keys = ft.AlertDialog(
-                title=ft.Text('EM BREVE!'),
-                content=ft.Column([
-                    ft.Card(ft.Row([full_screen, ft.Text('Tela cheia')], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), margin=1),
-                    ft.Card(ft.Row([return_home, ft.Text('Voltar para o home')], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), margin=1),
-                    ft.Card(ft.Row([next_page, ft.Text('Passar página')], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), margin=1),
-                    ft.Card(ft.Row([previous_page, ft.Text('Voltar página')], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), margin=1),
-                    
-                ])
-            )
-            page.dialog = change_keys
-            change_keys.open = True
-            page.update()
+        self.page = page
+        self.database = DataBase()
+        self.keybinds = self.database.get_config()['keybinds']
+        self.create_keybind_buttons()
 
-        self.content = ft.Row(
+    def create_keybind_buttons(self):
+        def listen_key(button, key):
+            button.text = '...'
+            self.page.update()
+
+        keybind_buttons = []
+        for key, label in self.keybinds.items():
+            button = ft.TextButton(label, disabled=True)
+            button.on_click = lambda event, btn=button, key=key: listen_key(btn, key)
+            keybind_buttons.append(button)
+
+        change_keys_content = ft.Column([
+            ft.Card(ft.Row([btn, ft.Text(label)], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), margin=1)
+            for btn, (key, label) in zip(keybind_buttons, self.keybinds.items())
+        ])
+
+        change_keys_dialog = ft.AlertDialog(
+            title=ft.Text('EM BREVE!'),
+            content=change_keys_content
+        )
+
+        self.page.dialog = change_keys_dialog
+        change_keys_dialog.open = True
+        self.page.update()
+
+    def return_content(self):
+        return ft.Row(
             [   
                 ft.Column([
-                    ft.TextButton('Mudar keybinds', on_click=change_keybind)
+                    ft.TextButton('Mudar keybinds', on_click=self.create_keybind_buttons)
                 ])
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             width=1300
         )
-
-    def return_content(self):
-        return self.content
